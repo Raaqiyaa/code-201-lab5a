9d8ce1e739adfb7b086dd72432c3e7997516ee67
raaqiya ali <raaqiyaali_@hotmail.co.uk>
2023-06-18 23:40:38 +0100

    update

--- js/app.js
+++ js/app.js
@@ -77,7 +77,6 @@ testSumAndMultiply(4,7,5);
 
 /////////////////////////////////////
 
-
 /* Problem 4
 Write a function called sumArray() that takes in an array of numbers as its single argument and then returns an array where the first element is the sum of the numbers in the array, and the second element is a string that EXACTLY follows this example and uses the values that were input into the function:
 
@@ -90,12 +89,15 @@ Test this function by hand in the console to get it working, and when you think 
 // Write your code here
 let testArray = [2, 3, 4]; 
 
-function sumArray(sumArr) {} 
-    let sumArray = 
+function sum(sumArr) {
+    let sum = sum (a,b,c)
+    let string3 = a + " " + b + " " + c + " was passed in array of numbers and" + 9 + " is their " sum " . ";
 
-// Here is the test for sumArray(); uncomment it to run it
+    return[sum3,string3]
 
-// testSumArray(testArray);
+}
+
+sumArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
